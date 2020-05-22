073612b4e1e28bfba37a874d0c69196c4530d31e
fall1600 <fall1600@gmail.com>
2020-05-22 23:26:50 +0800

    comment

--- Contracts/OrderInterface.php
+++ Contracts/OrderInterface.php
@@ -6,6 +6,9 @@ interface OrderInterface
 {
     /**
      * 交易金額
+     *  需為整數,不可有小數點和千位符號, 計價單位:新台幣。
+     *  (本參數內容以數字為主)
+     *  注意:使用 ATM 轉帳(虛擬帳號),若交易金額大於 3 萬元時需臨櫃匯款。
      * @return int
      */
     public function getAmount();
